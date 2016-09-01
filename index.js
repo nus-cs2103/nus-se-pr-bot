707070f3cc7bd0ab8f468e00c8f0e8f900c64889
Sam-Mauris Yong <mauris@hotmail.sg>
2016-09-01 11:26:07 +0800

    updated index to load se-edu repo

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ var initializer = require('./src/initializer');
 initializer(accuser, 'addressbook-level1');
 initializer(accuser, 'addressbook-level2');
 
+var seedu = require('./src/seedu');
+seedu(accuser, 'addressbook-level1');
+seedu(accuser, 'addressbook-level2');
+
 console.log ("Server has started");
 
 accuser
