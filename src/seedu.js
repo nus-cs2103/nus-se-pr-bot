819c5b8b06f7a1994d07037cec1086d24f69dde0
Sam-Mauris Yong <mauris@hotmail.sg>
2016-09-01 11:25:01 +0800

    added se-edu check

---
+++ src/seedu.js
@@ -0,0 +1,25 @@
+var config = require("config");
+var utility = require('../utility');
+var classMapping = config.get('classes');
+
+module.exports = function(accuser, repoName) {
+  var repo = accuser.addRepository('se-edu', repoName);
+
+  repo.newWorker()
+    .filter(function(repository, issue){
+      // ensure that we only work with PRs that do not have an assignee
+      return issue.pull_request;
+    })
+    .do(function(repository, issue) {
+      console.log("Looking at se-edu PR #" + issue.number);
+      var result = utility._titleRegex.exec(issue.title);
+
+      if (result === null) {
+        return;
+      }
+
+      console.log("Commenting and closing se-edu " + repoName + " PR #" + issue.number);
+      accuser.comment(repository, issue, "This PR should be against a repo in the org [nus-cs2103-AY1617S1](https://github.com/nus-cs2103-AY1617S1), not against the repo in se-edu org :-)\n\nClosing ...");
+      accuser.close(repository, issue);
+    });
+};
