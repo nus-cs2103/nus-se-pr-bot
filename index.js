d3bc4e5bfed89b0a6c7f514ce8af93e907b22936
Sam-Mauris Yong <mauris@hotmail.sg>
2016-09-10 10:35:13 +0800

    added level 3 repo processing

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ accuser.authenticate(githubAuthToken);
 var initializeSemesterRepositories = require('./src/semester');
 initializeSemesterRepositories(accuser, 'addressbook-level1');
 initializeSemesterRepositories(accuser, 'addressbook-level2');
+initializeSemesterRepositories(accuser, 'addressbook-level3');
 
 var intializeSeEduRepositories = require('./src/seedu');
 intializeSeEduRepositories(accuser, 'addressbook-level1');
 intializeSeEduRepositories(accuser, 'addressbook-level2');
+intializeSeEduRepositories(accuser, 'addressbook-level3');
 
 console.log ("Server has started");
 
