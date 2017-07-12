ea9729b3a74937153b00e9d6a364ab14273d8b07
Sam-Mauris Yong <mauris@hotmail.sg>
2017-07-12 21:20:25 +0800

    updated template

--- src/semester.js
+++ src/semester.js
@@ -1,27 +1,25 @@
 var utility = require('./utility');
 var classMapping = require('./mapping')['classes'];
+var mu = require('mu2');
+mu.root = __dirname + '/templates';
 
 let semesterAccount = 'nus-cs2103-AY1617S2';
 
 module.exports = (accuser, repoName) => {
+  mu.compile('format-check-request.mst');
+
   let warnInvalidTitle = (repository, issue) => {
     if (hasFormatCheckRequestedLabel(issue)) {
       return;
     }
+    let FormatCheckLabel = "FormatCheckRequested";
 
     console.log("Adding warning for format fail to PR #" + issue.number);
-    accuser.addLabels(repo, issue, ["FormatCheckRequested"]);
-    var comment = "Hi @" + issue.user.login + ", your pull request title is invalid."
-      + " It should be in the format of `[Activity ID][Team ID] Your name`,"
-      + " where `[Activity Id]` has no dashes or spaces (e.g. `[T2A3]` stands"
-      + " for Tutorial 2 Activity 3) and `[Team ID]` has one dash only and no"
-      + " spaces (e.g. `[W14-A2]` means Wednesday 2pm (14 hrs), Phase A, Team 2)."
-      + " Please follow the instructions given strictly and edit your title for reprocessing."
-      + "\n\nSubmit only one activity per pull request (unless otherwise stated in instructions) and"
-      + " do remember to create your branches from the commit where the `master` branch is pointing at so that each "
-      + " PR you submit only consist of commits meant for the activity."
-      + "\n\nNote: this comment is posted by a bot. If you believe this is done in error, please"
-      + " create an issue at [cs2103-pr-bot](https://github.com/mauris/cs2103-pr-bot/issues) and add a link to this PR."
+    accuser.addLabels(repo, issue, [FormatCheckLabel]);
+    let student = {
+      username: issue.user.login
+    };
+    var comment = mu.render('format-check-request.mst', student);
     accuser.comment(repository, issue, comment);
   };
 
@@ -46,7 +44,6 @@ module.exports = (accuser, repoName) => {
   };
 
   let repo = accuser.addRepository(semesterAccount, repoName);
-  let FormatCheckLabel = "FormatCheckRequested";
 
   repo.newWorker()
     .filter((repository, issue) => {
