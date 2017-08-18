e732e2469489ac2f468e30bdce428ae01d482c2f
Sam-Mauris Yong <mauris@hotmail.sg>
2017-08-18 17:16:41 +0800

    remove useless escape in regex

--- src/utility.js
+++ src/utility.js
@@ -1,4 +1,4 @@
 module.exports = {
-  _titleRegex: /^\[(\w+)\]\[([W|T|F]{1}\d{2})(\-[A|B|C]{1}(\d{1})){0,1}\]/i,
+  _titleRegex: /^\[(\w+)\]\[([W|T|F]{1}\d{2})(-[A|B|C]{1}(\d{1})){0,1}\]/i,
   _rcsTitleRegex: /^Add[ing|ed]{0,1}\s.+\.txt/i
 };
