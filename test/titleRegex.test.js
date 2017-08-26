668f82283bdaa46fdaa6a2dbc4ab404ab11a6547
Louis Lai <hoangdung2506@gmail.com>
2017-08-26 22:39:45 +0800

    Add more tests & allow double digit for learning outcome in titleRegex

--- test/titleRegex.test.js
+++ test/titleRegex.test.js
@@ -5,14 +5,14 @@ it('should validate valid titles', () => {
   expect(titleRegex.exec('[W2.2b][W09-A1]')).not.toBeNull();
   expect(titleRegex.exec('[W2.2][W09-A1]James Yong')).not.toBeNull();
   expect(titleRegex.exec('[W10.2][W09-A1]James Yong')).not.toBeNull();
+  expect(titleRegex.exec('[W10.10][W09-A1]James Yong')).not.toBeNull();
 });
 
 it('should invalidate invalid titles', () => {
-  expect(titleRegex.exec('')).toBeNull();
+  expect(titleRegex.exec('Learning Outcome 1')).toBeNull();
   expect(titleRegex.exec('W2.2b][W09-A1]')).toBeNull();
   expect(titleRegex.exec('[W2.2]')).toBeNull();
   expect(titleRegex.exec('[W09-A1]')).toBeNull();
-  expect(titleRegex.exec('[W10.10][W09-A1]James Yong')).toBeNull();
 });
 
 it('should extract classId & teamId from valid titles', () => {
