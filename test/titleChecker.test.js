3080d1fbb8b310eaded9d505e8481e6b00199aba
Gerald <ngjingsiang@gmail.com>
2017-08-29 23:00:25 +0800

    Add tests for TitleChecker

---
+++ test/titleChecker.test.js
@@ -0,0 +1,40 @@
+const Checker = require('../src/TitleChecker');
+
+describe('PR Title Checker ', () => {
+  it('should have a format check label', () => {
+    const checker = new Checker();
+    const label = 'FormatCheckRequested';
+    expect(checker.formatCheckLabel).toBe('FormatCheckRequested');
+  });
+
+  it('should have a regex pattern for matching title', () => {
+    const pattern = /some pattern/i;
+    const checker = new Checker(pattern);
+    expect(checker.pattern).toBeInstanceOf(RegExp);
+  });
+
+  it('should have undefined pattern by default', () => {
+    const checker = new Checker();
+    expect(checker.pattern).toBeUndefined();
+  });
+
+  it('should check for invalid format request labels', () => {
+    const checker = new Checker();
+    const labels = [
+      { name: 'randomlabel' },
+      { name: 'Format Check Requested' },
+      { name: 'check requested' },
+      { name: 'checkrequested' }
+    ];
+    expect(checker.hasCheckRequestLabel(labels)).toBeFalsy();
+  });
+
+  it('should check for valid format request labels', () => {
+    const checker = new Checker();
+    const labels = [
+      { name: 'FormatCheckRequested' },
+      { name: 'formatcheckrequested' }
+    ];
+    expect(checker.hasCheckRequestLabel(labels)).toBeTruthy();
+  });
+});
