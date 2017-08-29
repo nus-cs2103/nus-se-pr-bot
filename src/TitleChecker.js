94adee14be6acc1929ec87f554f8bbafcf82ea00
Gerald <ngjingsiang@gmail.com>
2017-08-29 23:01:16 +0800

    Abstract title checking into a class

---
+++ src/TitleChecker.js
@@ -0,0 +1,24 @@
+class TitleChecker {
+  constructor(pattern) {
+    this.formatCheckLabel = 'FormatCheckRequested';
+    this.pattern = pattern;
+  }
+
+  // Checks if label matches with the FormatCheckLabel
+  // Case insensitive.
+  hasCheckRequestLabel(labels) {
+    return labels.map(label => {
+                  return label.name.toLowerCase() === this.formatCheckLabel.toLowerCase();
+                })
+                .reduce((a, b) => a || b, false);
+  }
+
+  check(title) {
+    if (pattern === undefined) {
+      return
+    }
+    return this.pattern.exec(title);
+  }
+}
+
+module.exports = TitleChecker;
