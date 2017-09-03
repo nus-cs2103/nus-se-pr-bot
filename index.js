874eefcd55a12d5aba390c598769b05dd39d8cde
Louis Lai <hoangdung2506@gmail.com>
2017-09-03 11:35:13 +0800

    Handle PR from students' practice forks

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config({ silent: true });
 const utility = require('./src/utility');
 const Accuser = require('accuser');
 let currentLevel = require('./src/data')['currentLevel'];
+let semesterAccount = require('./src/data').semesterAccount;
 
 const accuser = new Accuser();
 
@@ -33,6 +34,15 @@ intializeSeEduRepositories(accuser, 'addressbook-level4', utility._titleRegex);
 
 // note that rcs repository has a different title regex string
 intializeSeEduRepositories(accuser, 'rcs', utility._rcsTitleRegex);
+
+// this section of code ensures that students do not send pull requests created for practice against
+// upstream se-edu and semester repo
+let initializePracticeForkRepository = require('./src/practiceFork');
+let practiceRepositories = ['samplerepo-things'];
+let practiceAccounts = ['se-edu', semesterAccount];
+practiceAccounts.forEach(account => {
+  practiceRepositories.forEach(repo => initializePracticeForkRepository(accuser, account, repo));
+});
 
 console.log("Bot Service has started");
 
