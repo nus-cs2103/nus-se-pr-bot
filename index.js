04501649b7c0de96afb6b738ea83e3ca3c4c9600
Gerald <ngjingsiang@gmail.com>
2017-09-26 22:57:54 +0800

    Fix lints

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ require('dotenv').config({ silent: true });
 const utility = require('./src/utility');
 const Accuser = require('accuser');
 
-let currentLevel = require('./config')['currentLevel'];
+let currentLevel = require('./config').currentLevel;
 let semesterAccount = require('./config').semesterAccount;
 
 const accuser = new Accuser({ interval: 600000 });
 
 const githubAuthToken = {
-  "type": "oauth",
-  "token": process.env.GITHUB_TOKEN
+  type: 'oauth',
+  token: process.env.GITHUB_TOKEN
 };
 
 accuser.authenticate(githubAuthToken);
 
 // Whitelisted
 const SubmissionRepos = require('./src/whitelist');
-for (var level = 1; level <= currentLevel; ++level) {
+for (var level = 1; level <= currentLevel; level++) {
   SubmissionRepos(accuser, `addressbook-level${level}`);
 }
 
@@ -38,7 +38,7 @@ const blackListedAccounts = [
 blackListedAccounts.forEach(account => {
   blackListedRepos.forEach(repo => {
     BlackListed(accuser, account, repo, 'practice-fork.mst');
-  })
+  });
 });
 
 
@@ -53,8 +53,8 @@ intializeSeEduRepositories(accuser, 'addressbook-level4', utility._titleRegex);
 // note that rcs repository has a different title regex string
 intializeSeEduRepositories(accuser, 'rcs', utility._rcsTitleRegex);
 
-console.log("Bot Service has started");
+console.log('Bot Service has started');
 
 
 // start the bot
-accuser.run({ assignee: "none" });
+accuser.run({ assignee: 'none' });
