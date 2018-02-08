335cbbc330c3256aaa2eae71980071db24f66bdb
Gerald <ngjingsiang@gmail.com>
2018-02-08 21:24:23 +0800

    Remove tutor header
    This header is unused

--- test/StudentMapping.test.js
+++ test/StudentMapping.test.js
@@ -1,54 +1,37 @@
 const StudentMapping = require('../src/StudentMapping');
 const path = require('path');
 const mapping = new StudentMapping(path.join(__dirname, '../data.sample.csv'));
+const louislai = {
+  'supervisor': 'madsonic',
+  'reviewer': 'mauris',
+  'labels': ['team.nonExistent', 'tutorial.nonExistent']
+};
+const phua = {
+  'supervisor': '',
+  'reviewer': 'OuyangDanwen',
+  'labels': ['team.A2', 'tutorial.T11']
+};
+const pokka = {
+  'supervisor': '',
+  'reviewer': 'OuyangDanwen',
+  'labels': ['team.A1', 'tutorial.T11']
+}
+const mario = {
+  'supervisor': 'Aquaman',
+  'reviewer': 'Superman',
+  'labels': ['team.A2', 'tutorial.T11']
+}
 
 it('should extract data correctly from multiple lines', () => {
-  const louislai = {
-    'tutor': 'madsonic',
-    'supervisor': '',
-    'reviewer': 'mauris',
-    'labels': ['team.nonExistent', 'tutorial.nonExistent']
-  };
-  const phua = {
-    'tutor': '',
-    'supervisor': '',
-    'reviewer': 'OuyangDanwen',
-    'labels': ['team.A2', 'tutorial.T11']
-  };
-  const pokka = {
-    'tutor': '',
-    'supervisor': '',
-    'reviewer': 'OuyangDanwen',
-    'labels': ['team.A1', 'tutorial.T11']
-  }
-  const mario = {
-    'tutor': 'Aquaman',
-    'supervisor': 'Superman',
-    'reviewer': '',
-    'labels': ['team.A2', 'tutorial.T11']
-  }
   const data = { louislai, phua, pokka, mario };
   expect(mapping.data).toEqual(data);
 });
 
 it('should extract data correctly when all fields are given', () => {
-  const louislai = {
-    'tutor': 'madsonic',
-    'supervisor': '',
-    'reviewer': 'mauris',
-    'labels': ['team.nonExistent', 'tutorial.nonExistent']
-  };
-
   expect(mapping.getInfoForStudent('louislai')).toEqual(louislai);
 });
 
 it('should extract data correctly when some fields are missing', () => {
-  const pokka = {
-    'tutor': '',
-    'supervisor': '',
-    'reviewer': 'OuyangDanwen',
-    'labels': ['team.A1', 'tutorial.T11']
-  }
   expect(mapping.getInfoForStudent('pokka')).toEqual(pokka);
 });
 
