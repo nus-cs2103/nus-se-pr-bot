e8f17d9c0f8c46eb7513204af0a15e26b8a4c0c5
Lai Hoang Dung (Louis) <hoangdung2506@gmail.com>
2018-08-11 11:35:36 +0800

    Config switcher for different semester modules

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // Load dotenv first
 require('dotenv').config({ silent: true });
-const path = require('path');
 const Promise = require('bluebird');
 const SubmissionRepos = require('./src/Whitelist');
+const Config = require('./src/Config');
 const Blacklisted = require('./src/Blacklist');
 const Greylisted = require('./src/Greylist');
 const Accuser = require('accuser');
 const Validator = require('./src/Validator');
 const StudentMapping = require('./src/StudentMapping');
-const phaseMapping = new StudentMapping(path.join(__dirname, './data.csv'));
-const currentLevel = require('./config').currentLevel;
-const semesterAccount = require('./config').semesterAccount;
-const originAccount = require('./config').originAccount;
+
+const config = new Config();
+const originAccount = config.originAccount;
+const modules = config.modules;
 const maxLevel = 4;
+
 
 // interval in miliseconds
 // 10 min interval
@@ -57,19 +58,25 @@ blackListedOriginRepos.forEach(repoName => {
   repoPromises.push(repo[runMethod]());
 });
 
-blackListedSemesterRepos.forEach(repoName => {
-  const validator = new Validator(accuser, semesterAccount, repoName);
-  const repo = new Blacklisted(accuser, semesterAccount, repoName, validator);
-  repoPromises.push(repo[runMethod]());
-});
+Object.values(modules).forEach(module => {
+  const { moduleConfig: [semesterAccount, currentLevel], studentMappingPath } = module;
+  const phaseMapping = new StudentMapping(studentMappingPath);
 
-// Whitelisted
-for (let level = 1; level <= currentLevel; level += 1) {
-  const repoName = `addressbook-level${level}`;
-  const validator = new Validator(accuser, semesterAccount, repoName);
-  const repo = new SubmissionRepos(accuser, semesterAccount, repoName, validator, phaseMapping);
-  repoPromises.push(repo[runMethod]());
-}
+  blackListedSemesterRepos.forEach(repoName => {
+    const validator = new Validator(accuser, semesterAccount, repoName);
+    const repo = new Blacklisted(accuser, semesterAccount, repoName, validator);
+    repoPromises.push(repo[runMethod]());
+  });
+
+
+  // Whitelisted
+  for (let level = 1; level <= currentLevel; level += 1) {
+    const repoName = `addressbook-level${level}`;
+    const validator = new Validator(accuser, semesterAccount, repoName);
+    const repo = new SubmissionRepos(accuser, semesterAccount, repoName, validator, phaseMapping);
+    repoPromises.push(repo[runMethod]());
+  }
+});
 
 Promise.all(repoPromises).then(() => {
   if (!isDryRun) {
